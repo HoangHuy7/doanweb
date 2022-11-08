4e44becf159358ee0214f2c9b8e58ab991c4f8d2
hoang huy <hoanghuy200701@gmail.com>
2022-11-08 10:07:43 +0700

    first commit

---
+++ DOAN_TH/ProductDTO.php
@@ -0,0 +1,19 @@
+<?php
+class ProductDTO
+{
+    public $id;
+    public $name;
+    public $price;
+    public $content;
+    public $image;
+    
+    function __construct()
+    {
+        
+    }
+
+    function __toString()
+    {
+        return "{"."\"id\":\"$this->id\"".",\"name\":\"$this->name\"" .",\"price\":\"$this->price\"".",\"content\":\"$this->price\"".",\"image\":\"$this->image\""."}";
+    }
+}
---
+++ DOAN_TH/product.php
@@ -0,0 +1,83 @@
+<?php
+header("Access-Control-Allow-Origin: *"); //add this CORS header to enable any domain to send HTTP requests to these endpoints:
+include "ProductDTO.php";
+
+
+
+
+$id = "";
+$sql = "";
+$conn = new PDO(
+    'mysql:host=sql.freedb.tech;dbname=freedb_doanweb',
+    "freedb_doanweb",
+    "bckKv#&nUTjtW76",
+    array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+);
+$method = $_SERVER['REQUEST_METHOD'];
+switch ($method) {
+    case 'GET':
+        $sql = "select * from product";
+        $stmt = $conn->prepare($sql);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, "ProductDTO");
+        $stmt->execute();
+        if (!$id) echo '[';
+        $t = 0;
+        while ($row = $stmt->fetch()) {
+
+            echo ($t > 0 ? ',' : '') . $row;
+            $t++;
+        }
+        if (!$id) echo ']';
+        break;
+    case 'POST':
+        $message = ["message" => "false"];
+        try {
+
+            $sql = "INSERT INTO product ( name, price, content, image) VALUES ( :name, :price, :content, :image);";
+            $stmt = $conn->prepare($sql);
+            if ($stmt->execute($_POST)) $message = ["message" => "success"];
+            echo json_encode($message);
+        } catch (Exception $e) {
+            $pdo->rollback();
+            $message = ["message" => $e];
+            echo json_encode($message);
+            throw $e;
+        }
+        break;
+    case 'DELETE': // de delete
+        $message = ["message" => "false"];
+        try {
+            $sql = "SELECT * FROM `product` WHERE id = :id";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':id', $_REQUEST["id"], PDO::PARAM_INT);
+            $stmt->execute();
+            while ($stmt->fetch()) {
+                $sql = "DELETE FROM `product` WHERE `product`.`id` = :id";
+                $stmt = $conn->prepare($sql);
+                $stmt->bindParam(':id', $_REQUEST["id"], PDO::PARAM_INT);
+                if ($stmt->execute()) {
+                    $message = ["message" => "true"];
+                }
+            }
+            echo json_encode($message);
+        } catch (Exception $e) {
+            $pdo->rollback();
+            echo $e;
+            throw $e;
+        }
+        break;
+    case 'PUT': // de sua
+        $message = ["message" => "false"];
+        try {
+            $sql = "UPDATE product SET name = :name, price = :price, content = :content, image = :image WHERE id = :id;";
+            $stmt = $conn->prepare($sql);
+            if ($stmt->execute($_REQUEST))  $message = ["message" => "true"];
+            echo json_encode($message);
+           
+        } catch (Exception $e) {
+            $pdo->rollback();
+            echo $e;
+            throw $e;
+        }
+        break;
+}

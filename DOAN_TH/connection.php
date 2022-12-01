aadf7ff2c443a952e221e5f72275b3aaeb19ec7e
hoang huy <hoanghuy200701@gmail.com>
2022-12-01 22:32:05 +0700

    haha

--- DOAN_TH/connection.php
+++ DOAN_TH/connection.php
@@ -3,9 +3,9 @@
     function getConnection(){
         try {
             $conn = new PDO(
-                'mysql:host=localhost;dbname=buoi7',
-                "root",
-                "",
+                'mysql:host=sql.freedb.tech;dbname=freedb_doanwebhuy',
+                "freedb_doanannghia",
+                "xSPuqUE?&2k3aZN",
                 array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
             );
             return $conn;
